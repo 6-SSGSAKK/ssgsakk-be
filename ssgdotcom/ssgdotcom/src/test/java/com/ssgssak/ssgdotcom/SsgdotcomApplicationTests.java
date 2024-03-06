728930643a7982ddfa580cbb9bf332af3a342186
hoontaepark <pthomj13@gmail.com>
2024-03-06 16:06:49 +0900

    firstsetting

---
+++ ssgdotcom/ssgdotcom/src/test/java/com/ssgssak/ssgdotcom/SsgdotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssgssak.ssgdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgdotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
