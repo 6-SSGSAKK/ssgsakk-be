0ef395891ca3d27044bc9619c673abc0fb2a1717
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-06 16:37:09 +0900

    Add: first commit

---
+++ src/test/java/com/ssgsakk/ssgdotcom/SsgdotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssgsakk.ssgdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgdotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
