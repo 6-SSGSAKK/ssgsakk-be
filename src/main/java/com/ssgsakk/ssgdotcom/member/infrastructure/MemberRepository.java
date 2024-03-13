bca605ff9d27e8c72965d4157a50922febe07876
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-13 15:13:47 +0900

    Feat: Id와 비밀번호로 로그인 기능 구현

--- src/main/java/com/ssgsakk/ssgdotcom/member/infrastructure/MemberRepository.java
+++ src/main/java/com/ssgsakk/ssgdotcom/member/infrastructure/MemberRepository.java
@@ -1,4 +1,10 @@
 package com.ssgsakk.ssgdotcom.member.infrastructure;
 
-public class MemberRepository {
+import com.ssgsakk.ssgdotcom.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+   Member findByUserIdAndUserPassword(String userId, String userPassword);
 }
