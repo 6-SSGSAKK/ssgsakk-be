dbd8ab7a2c20a68a41a6aeab225d476861773027
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-13 15:16:12 +0900

    Feat: 로그인 기능 구현

--- src/main/java/com/ssgsakk/ssgdotcom/member/presentation/AuthController.java
+++ src/main/java/com/ssgsakk/ssgdotcom/member/presentation/AuthController.java
@@ -1,4 +1,43 @@
 package com.ssgsakk.ssgdotcom.member.presentation;
 
+import com.ssgsakk.ssgdotcom.member.application.AuthService;
+import com.ssgsakk.ssgdotcom.member.dto.SignInDto;
+import com.ssgsakk.ssgdotcom.member.vo.SignInRequestVo;
+import com.ssgsakk.ssgdotcom.member.vo.SignInResponseVo;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "로그인", description = "로그인", tags = { "User SignIn" })
+    @PostMapping("signin")
+    public ResponseEntity<SignInResponseVo> signIn(@RequestBody SignInRequestVo signInRequestVo) {
+
+        SignInDto signInDto = SignInDto.builder()
+                .userId(signInRequestVo.getUserId())
+                .userPassword(signInRequestVo.getUserPassword())
+                .build();
+
+        signInDto = authService.signIn(signInDto);
+
+        SignInResponseVo signInResponseVo = SignInResponseVo.builder()
+                .userName(signInDto.getUserName())
+                .uuid(signInDto.getUuid())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(signInResponseVo);
+    }
 }
