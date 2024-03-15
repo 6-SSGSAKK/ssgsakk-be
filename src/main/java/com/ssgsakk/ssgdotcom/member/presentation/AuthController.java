d85337670768d98f142256147ae4d1789d6df35b
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-15 16:44:51 +0900

    Refactor: 강사님 피드백

--- src/main/java/com/ssgsakk/ssgdotcom/member/presentation/AuthController.java
+++ src/main/java/com/ssgsakk/ssgdotcom/member/presentation/AuthController.java
@@ -1,13 +1,10 @@
 package com.ssgsakk.ssgdotcom.member.presentation;
 
-import com.ssgsakk.ssgdotcom.common.exception.BusinessException;
-import com.ssgsakk.ssgdotcom.common.exception.ErrorCode;
 import com.ssgsakk.ssgdotcom.common.response.BaseResponse;
 import com.ssgsakk.ssgdotcom.member.application.AuthService;
 import com.ssgsakk.ssgdotcom.member.dto.SignInDto;
 import com.ssgsakk.ssgdotcom.member.dto.SignUpDto;
-//import com.ssgsakk.ssgdotcom.member.error.CustomException;
-//import com.ssgsakk.ssgdotcom.member.error.ErrorCode;
+
 import com.ssgsakk.ssgdotcom.member.vo.SignInRequestVo;
 import com.ssgsakk.ssgdotcom.member.vo.SignInResponseVo;
 import com.ssgsakk.ssgdotcom.member.vo.SignUpRequestVo;
@@ -15,8 +12,6 @@ import com.ssgsakk.ssgdotcom.member.vo.SignUpResponseVo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,19 +34,12 @@ public class AuthController {
                 .userPassword(signInRequestVo.getUserPassword())
                 .build();
 
-        signInDto = authService.signIn(signInDto);
+        SignInDto servicedSignInDto = authService.signIn(signInDto);
 
-        // uuid를 통해 계정 유무 확인
-        if (signInDto.getUuid() == null || signInDto.getUuid().isEmpty()) {
-            throw new BusinessException(ErrorCode.TEST_ERROR_CODE);
-        }
-
-        SignInResponseVo signInResponseVo = SignInResponseVo.builder()
-                .userName(signInDto.getUserName())
-                .uuid(signInDto.getUuid())
-                .build();
-
-        return new BaseResponse<>(signInResponseVo);
+        return new BaseResponse<>(SignInResponseVo.builder()
+                .userName(servicedSignInDto.getUserName())
+                .uuid(servicedSignInDto.getUuid())
+                .build());
     }
 
     @Operation(summary = "회원가입", description = "회원가입", tags = {"User SignUp"})
@@ -67,13 +55,12 @@ public class AuthController {
                 .userMobileNum(signUpRequestVo.getUserMobileNum())
                 .build();
 
-        signUpDto = authService.signUp(signUpDto);
+        SignUpDto servicedSignUpDto = authService.signUp(signUpDto);
 
-        SignUpResponseVo signUpResponseVo = SignUpResponseVo.builder()
-                .userName(signUpDto.getUserName())
-                .uuid(signUpDto.getUuid())
-                .build();
-        return new BaseResponse<>(signUpResponseVo);
+        return new BaseResponse<>(SignUpResponseVo.builder()
+                .userName(servicedSignUpDto.getUserName())
+                .uuid(servicedSignUpDto.getUuid())
+                .build());
 
     }
 }
