fd5aba49a3ef4478af016b695494e35fde463f8f
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-18 09:37:27 +0900

    Refactor: BaseResponseEntity의 isSuccessed 필드 삭제

--- src/main/java/com/ssgsakk/ssgdotcom/common/response/BaseResponse.java
+++ src/main/java/com/ssgsakk/ssgdotcom/common/response/BaseResponse.java
@@ -1,14 +1,14 @@
 package com.ssgsakk.ssgdotcom.common.response;
 
-public record BaseResponse<T>(Boolean isSuccess, T ResponseVo) {
+public record BaseResponse<T>(T ResponseVo) {
 
     // 요청에 성공한 경우 -> return 객체가 필요한 경우
     public BaseResponse(T ResponseVo) {
-        this(true,  ResponseVo);
+        this.ResponseVo = ResponseVo;
     }
 
     // 요청에 성공한 경우 -> return 객체가 필요 없는 경우
-    public BaseResponse() {
-        this(true, null);
-    }
+//    public BaseResponse() {
+//        this(null);
+//    }
 }
