7bff5bf73ab72abff58c8f00145c3a57a0f797df
icarus012832 <icarus012832@gmail.com>
2024-03-18 15:39:17 +0900

    Refactor: 예외처리 적용

--- src/main/java/com/ssgsakk/ssgdotcom/product/presentation/ProductController.java
+++ src/main/java/com/ssgsakk/ssgdotcom/product/presentation/ProductController.java
@@ -1,5 +1,6 @@
 package com.ssgsakk.ssgdotcom.product.presentation;
 
+import com.ssgsakk.ssgdotcom.common.response.BaseResponse;
 import com.ssgsakk.ssgdotcom.product.Vo.*;
 import com.ssgsakk.ssgdotcom.product.application.ProductService;
 import com.ssgsakk.ssgdotcom.product.dto.AddProductDto;
@@ -11,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,42 +27,41 @@ public class ProductController {
 
     // 상품 검색
     @GetMapping("/search")
-    public ResponseEntity<?> searchProducts(@RequestParam("keyword") String keyword) {
+    public BaseResponse<?> searchProducts(@RequestParam("keyword") String keyword) {
         List<SearchProductDto> searchProductDto = productService.searchProducts(keyword);
 
-        List<SearchProductResponseVo> searchResultVos = searchProductDto.stream()
+
+        return new BaseResponse<>("searchProduct Success", searchProductDto.stream()
                 .map(productDto -> SearchProductResponseVo.builder()
                         .productSeq(productDto.getProductSeq())
                         .productName(productDto.getProductName())
                         .productPrice(productDto.getProductPrice())
                         .build())
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(searchResultVos);
+                .collect(Collectors.toList()));
     }
 
 
     // 상품 상세 정보 조회
     @GetMapping("/{id}")
     @Operation(summary = "상품 상세 정보 조회", description = "상품의 상세 정보를 조회", tags = { "Product Info" })
-    public ResponseEntity<?> ProductInfo(@PathVariable("id") Long id) {
+    public BaseResponse<?> productInfo(@PathVariable("id") Long id) {
         ProductDto productDto = productService.productInfo(id);
-        ProductInfoResponseVo productInfoResponseVo = ProductInfoResponseVo.builder()
+
+        return new BaseResponse<>("productInfo Success", ProductInfoResponseVo.builder()
                 .productName(productDto.getProductName())
                 .productPrice(productDto.getProductPrice())
                 .vendor(productDto.getVendor())
                 .productCode(productDto.getProductCode())
                 .productDescription(productDto.getProductDescription())
                 .discountPercent(productDto.getDiscountPercent())
-                .build();
-        return ResponseEntity.ok(productInfoResponseVo);
+                .build());
     }
 
 
     // 상품 등록
     @PostMapping()
     @Operation(summary = "상품 등록", description = "새로운 상품을 등록", tags = { "Add product" })
-    public ResponseEntity<?> addProduct(@RequestBody AddProductRequestVo addProductRequestVo) {
+    public BaseResponse<?> addProduct(@RequestBody AddProductRequestVo addProductRequestVo) {
         AddProductDto addProductRequestDto = AddProductDto.builder()
                 .productName(addProductRequestVo.getProductName())
                 .productPrice(addProductRequestVo.getProductPrice())
@@ -73,15 +72,14 @@ public class ProductController {
                 .build();
         AddProductDto addProductResponseDto = productService.addProduct(addProductRequestDto);
 
-        return ResponseEntity.ok(AddProductResponseVo.builder()
-                .productName(addProductResponseDto.getProductName())
-                .build());
+        return new BaseResponse<>("addProduct Success", AddProductResponseVo.builder()
+                .productName(addProductResponseDto.getProductName()));
     }
 
     // 상품 수정
     @PutMapping("/{id}")
     @Operation(summary = "상품 수정", description = "기존 상품을 수정", tags = { "Update product" })
-    public ResponseEntity<?> updateProduct(
+    public BaseResponse<?> updateProduct(
             @PathVariable("id") Long id,
             @RequestBody UpdateProductRequestVo updateProductRequestVo) {
         UpdateProductDto updateProductDto = UpdateProductDto.builder()
@@ -93,15 +91,15 @@ public class ProductController {
                 .discountPercent(updateProductRequestVo.getDiscountPercent())
                 .build();
         productService.updateProduct(id, updateProductDto);
-        return ResponseEntity.ok("");
+        return new BaseResponse<>("updateProduct Success", "");
     }
 
 
     // 상품 삭제
     @DeleteMapping("/{id}")
     @Operation(summary = "상품 삭제", description = "상품 삭제", tags = { "Delete product" })
-    public ResponseEntity<?> deleteProduct(@PathVariable("id") Long id) {
+    public BaseResponse<?> deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProduct(id);
-        return ResponseEntity.ok("");
+        return new BaseResponse<>("deleteProduct Success", "");
     }
 }
