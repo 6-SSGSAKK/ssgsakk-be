c8cfed2d5927024299d69f8a0c053763650068f6
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-29 11:43:18 +0900

    Test: auth test

--- src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
+++ src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(
-                                "api/v1/auth/**"
+                                "/api/v1/auth/**"
                                 ,"/api/v1/auth/signin"
                                 , "/api/v1/auth/signup"
                                 , "/api/v1/auth/mail-send"
