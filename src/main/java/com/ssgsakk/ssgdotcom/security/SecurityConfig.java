057cad04e326a35c231eb91b4e2ea379f1a1c813
chanchanwoong <cksdnddlek99@gmail.com>
2024-03-29 11:40:50 +0900

    Test: config test

--- src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
+++ src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
@@ -90,7 +90,9 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/v1/auth/signin"
+                        .requestMatchers(
+                                "api/vi/auth/**"
+                                ,"/api/v1/auth/signin"
                                 , "/api/v1/auth/signup"
                                 , "/api/v1/auth/mail-send"
                                 , "/api/v1/auth/mail-check"
