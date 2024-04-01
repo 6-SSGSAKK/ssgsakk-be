a6c8765a2ac10f445104119c71a32013ee3fc7c0
chanchanwoong <cksdnddlek99@gmail.com>
2024-04-01 14:48:57 +0900

    test: cors

--- src/main/java/com/ssgsakk/ssgdotcom/config/CorsConfig.java
+++ src/main/java/com/ssgsakk/ssgdotcom/config/CorsConfig.java
@@ -1,21 +1,21 @@
-//package com.ssgsakk.ssgdotcom.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    // CORS 설정
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods(CorsConfiguration.ALL)
-//                .allowedHeaders(CorsConfiguration.ALL)
-//                .allowedOriginPatterns(CorsConfiguration.ALL)
-//                .exposedHeaders(CorsConfiguration.ALL)
-//                .allowCredentials(false);
-//    }
-//}
+package com.ssgsakk.ssgdotcom.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    // CORS 설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("https://ssgsakk-fe.vercel.app/", "http://localhost:3000", "https://www.ssgssak.shop/")
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedOriginPatterns(CorsConfiguration.ALL)
+                .exposedHeaders(CorsConfiguration.ALL)
+                .allowCredentials(false);
+    }
+}
--- src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
+++ src/main/java/com/ssgsakk/ssgdotcom/security/SecurityConfig.java
@@ -33,28 +33,28 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         // cors 설정
-        http
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-                        CorsConfiguration configuration = new CorsConfiguration();
-
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                        configuration.setAllowedOrigins(Collections.singletonList("https://ssgsakk-fe.vercel.app/"));
-                        configuration.setAllowedOrigins(Collections.singletonList("https://www.ssgssak.shop/"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
-
-                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-
-                        return configuration;
-                    }
-                }));
+//        http
+//                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+//
+//                    @Override
+//                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//
+//                        CorsConfiguration configuration = new CorsConfiguration();
+//
+//                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//                        configuration.setAllowedOrigins(Collections.singletonList("https://ssgsakk-fe.vercel.app/"));
+//                        configuration.setAllowedOrigins(Collections.singletonList("https://www.ssgssak.shop/"));
+//                        configuration.setAllowedMethods(Collections.singletonList("*"));
+//                        configuration.setAllowCredentials(true);
+//                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                        configuration.setMaxAge(3600L);
+//
+//                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+//                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//
+//                        return configuration;
+//                    }
+//                }));
 
 
 
