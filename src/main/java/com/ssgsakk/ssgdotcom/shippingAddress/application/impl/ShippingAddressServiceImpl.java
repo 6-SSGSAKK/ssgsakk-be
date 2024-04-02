f13f02bc05a8a89064dfcc391bc141d68fb7c715
chanchanwoong <cksdnddlek99@gmail.com>
2024-04-02 11:51:22 +0900

    Feat: 기본 배송지 변경 구현

--- src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/application/impl/ShippingAddressServiceImpl.java
+++ src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/application/impl/ShippingAddressServiceImpl.java
@@ -1,16 +1,30 @@
 package com.ssgsakk.ssgdotcom.shippingAddress.application.impl;
 
+import com.ssgsakk.ssgdotcom.member.domain.User;
+import com.ssgsakk.ssgdotcom.member.infrastructure.MemberRepository;
 import com.ssgsakk.ssgdotcom.shippingAddress.application.ShippingAddressService;
 import com.ssgsakk.ssgdotcom.shippingAddress.dto.ChangeDefaultAddressDto;
 import com.ssgsakk.ssgdotcom.shippingAddress.infrastructure.ShippingAddressRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@Slf4j
 @RequiredArgsConstructor
 public class ShippingAddressServiceImpl implements ShippingAddressService {
-    private ShippingAddressRepository shippingAddressRepository;
+    private final ShippingAddressRepository shippingAddressRepository;
+    private final MemberRepository memberRepository;
 
     @Override
+    @Transactional
     public void changeDefaultAddress(ChangeDefaultAddressDto changeDefaultAddressDto) {
-        shippingAddressRepository.changeDefaultAddress(changeDefaultAddressDto.getUuid(), changeDefaultAddressDto.getShippingAddressSeq());
+        // 기존 기본 shippinAddressSeq만 0으로 변경하고 전달받은 shippingAddressSeq만 1로 바꾸고 나머지는 전부 0으로 변환
+        User user = memberRepository.findByUuid(changeDefaultAddressDto.getUuid()).get();
+        shippingAddressRepository.setDefaultAddress(user);
+        shippingAddressRepository.changeDefaultAddress(changeDefaultAddressDto.getShippingAddressSeq());
     }
 }
--- src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/infrastructure/ShippingAddressRepository.java
+++ src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/infrastructure/ShippingAddressRepository.java
@@ -1,15 +1,25 @@
 package com.ssgsakk.ssgdotcom.shippingAddress.infrastructure;
 
+import com.ssgsakk.ssgdotcom.member.domain.User;
 import com.ssgsakk.ssgdotcom.shippingAddress.domain.ShippingAddress;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShippingAddressRepository extends JpaRepository<ShippingAddress, Long> {
 
+    // 기존 기본 배송지 value를 0으로 변환
     @Transactional
     @Modifying
-    @Query("UPDATE User u SET userPassword = :password WHERE u.uuid = :uuid")
-    void changeDefaultAddress(String uuid, Long shippingAddressSeq);
+    @Query("UPDATE ShippingAddress sa SET sa.defaultAddressCheck = 0 WHERE sa.user = :user")
+    void setDefaultAddress(@Param("user") User user);
+
+    // 지정받은 배송지 seq를 1로 변환
+    @Transactional
+    @Modifying
+    @Query("UPDATE ShippingAddress sa SET sa.defaultAddressCheck = 1 WHERE sa.shippingAddressSeq = :shippingAddressSeq")
+    void changeDefaultAddress(@Param("shippingAddressSeq") Long shippingAddressSeq);
+
 }
