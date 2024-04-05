cac7412ae1b8667779987787d56a1de59a9d8c4f
chanchanwoong <cksdnddlek99@gmail.com>
2024-04-05 10:47:51 +0900

    Test: encoding test

--- src/main/java/com/ssgsakk/ssgdotcom/oauth2/CustomSuccessHandler.java
+++ src/main/java/com/ssgsakk/ssgdotcom/oauth2/CustomSuccessHandler.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 
 @Component
@@ -47,8 +48,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
             // 적절한 HTTP 상태 코드 설정
             response.setStatus(HttpServletResponse.SC_OK);
-//            response.sendRedirect("http://localhost:3000/login/social?token=" + "Bearer " + token + "&state=0&userName=" + user.getName() + "&userEmail=" + customUserDetails.getEmail());
-            response.sendRedirect("http://localhost:3000/login/social" + "?state=2&userEmail=" + customUserDetails.getEmail() + "&oAuthId=" + oauthId);
+            response.sendRedirect("http://localhost:3000/login/social?token=" + "Bearer " + token + "&state=0&userName=" + URLEncoder.encode(user.getName(), "UTF-8") + "&userEmail=" + customUserDetails.getEmail());
+//            response.sendRedirect("http://localhost:3000/login/social" + "?state=2&userEmail=" + customUserDetails.getEmail() + "&oAuthId=" + oauthId);
 
         }
         // oauthId가 없는 사람들 중, user 테이블에 동일 이메일이 있는 지 확인
@@ -68,8 +69,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
                 // 적절한 HTTP 상태 코드 설정
                 response.setStatus(HttpServletResponse.SC_ACCEPTED);
-//                response.sendRedirect("http://localhost:3000/login/social" + "?token=" + "Bearer " + token + "&state=1&userName=" + user.getName() + "&userEmail=" + customUserDetails.getEmail());
-                response.sendRedirect("http://localhost:3000/login/social" + "?state=2&userEmail=" + customUserDetails.getEmail() + "&oAuthId=" + oauthId);
+                response.sendRedirect("http://localhost:3000/login/social" + "?token=" + "Bearer " + token + "&state=1&userName=" + URLEncoder.encode(user.getName(), "UTF-8") + "&userEmail=" + customUserDetails.getEmail());
+//                response.sendRedirect("http://localhost:3000/login/social" + "?state=2&userEmail=" + customUserDetails.getEmail() + "&oAuthId=" + oauthId);
 
             }
             // 동일 이메일이 없는 경우
