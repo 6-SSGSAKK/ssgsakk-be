0124384b7deb8cc8c78f0e78d5f1bab176915142
icarus012832 <icarus012832@gmail.com>
2024-04-09 16:32:39 +0900

    Fix: 장바구니 상세 조회시 재고도 같이 가도록 수정

--- src/main/java/com/ssgsakk/ssgdotcom/cart/dto/CartInfoDto.java
+++ src/main/java/com/ssgsakk/ssgdotcom/cart/dto/CartInfoDto.java
@@ -6,12 +6,17 @@ import lombok.Getter;
 @Getter
 @Builder
 public class CartInfoDto {
+    private Long cartSeq;
     private Long productSeq;
     private String productOption;
+    private Long OptionAndStockSeq;
+    private Integer stock;
     private String productVendor;
     private Integer productPrice;
     private String productName;
     private Integer quantity;
     private String productImage;
     private Integer discountPercent;
+    private Integer fixItem;
+    private Integer checkbox;
 }
