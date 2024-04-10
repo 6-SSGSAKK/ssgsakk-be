fb7bdc464e7bedec1fc9ca511d4ac6297494c962
hoontaepark <pthomj13@gmail.com>
2024-04-10 16:24:36 +0900

    draft

--- src/main/java/com/ssgsakk/ssgdotcom/purchaseproduct/infrastructure/PurchaseProductRepositoryImp.java
+++ src/main/java/com/ssgsakk/ssgdotcom/purchaseproduct/infrastructure/PurchaseProductRepositoryImp.java
@@ -1,6 +1,6 @@
 package com.ssgsakk.ssgdotcom.purchaseproduct.infrastructure;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssgsakk.ssgdotcom.option.domain.OptionAndStock;
 import com.ssgsakk.ssgdotcom.purchaseproduct.domain.PurchaseProduct;
 import com.ssgsakk.ssgdotcom.purchaseproduct.domain.QPurchaseProduct;
 import com.ssgsakk.ssgdotcom.purchaseproduct.dto.PurchaseProductStateDto;
@@ -33,7 +33,7 @@ public class PurchaseProductRepositoryImp extends QuerydslRepositorySupport {
 
     }
 
-    @Transactional
+    @Transactional //주문상태변경 메소드
     public void updateProductState(Long purchaseProductSeq, PurchaseProductStateDto purchaseProductStateDto)  {
 
         long updateProductState = jpaQueryFactory.update(qPurchaseProduct)
@@ -45,13 +45,36 @@ public class PurchaseProductRepositoryImp extends QuerydslRepositorySupport {
             throw new RuntimeException("존재하지 않는 상태번호");
 
         } else if (updateProductState == 5){
-            increaseProductStock(purchaseProductSeq);
+            crateproductSeqAndPurchaseProductCount(purchaseProductSeq);
         }
     }
 
-    public void increaseProductStock(Long purchaseProductSeq){
+    @Transactional //if주문상태5(주문상태) purchaseProductSeq 별 productSeq,PurchaseProductCount 값 추출
+    public void crateproductSeqAndPurchaseProductCount(Long purchaseProductSeq){
 
+        PurchaseProduct purchaseProduct = jpaQueryFactory
+                .select(Projections.fields(PurchaseProduct.class, qPurchaseProduct.productSeq,
+                        qPurchaseProduct.purchaseProductCount))
+                .from(qPurchaseProduct)
+                .where(qPurchaseProduct.purchaseProductSeq.eq(purchaseProductSeq))
+                .fetchOne();
 
+        Long productSeq = purchaseProduct.getProductSeq(); //PathVariable로 받은 purchaseProductSeq 에 해당하는 productSeq
+        Integer purchaseProductCount = purchaseProduct.getPurchaseProductCount();
+        //PathVariable로 받은 purchaseProductSeq 에 해당하는 PurchaseProductCount
+
+        increaseStock(productSeq, purchaseProductCount);
+        //productSeq, purchaseProductCount 인자값으로 넘겨줌
+
+    }
+
+    @Transactional //추출된 ProductSeq,PurchaseProductCount를 이용해 취소된 수량만큼 재고증가
+    public void increaseStock(Long productSeq, Integer purchaseProductCount){
+
+        Long increaseStock = jpaQueryFactory.update(optionAndStock)
+                .set(optionAndStock.stock, optionAndStock.stock.add(purchaseProductCount))
+                .where(optionAndStock.productSeq.eq(productSeq))
+                .execute();
     }
 
 
