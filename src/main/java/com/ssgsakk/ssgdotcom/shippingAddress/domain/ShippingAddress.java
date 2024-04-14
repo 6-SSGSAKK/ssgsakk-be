d3e529fea2963cf70f735c62f6d49c5b307f0d05
chanchanwoong <cksdnddlek99@gmail.com>
2024-04-14 18:25:01 +0900

    Test: addressNickname test

--- src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/domain/ShippingAddress.java
+++ src/main/java/com/ssgsakk/ssgdotcom/shippingAddress/domain/ShippingAddress.java
@@ -20,21 +20,21 @@ public class ShippingAddress extends BaseTimeEntity {
     @Column(nullable = false)
     private String uuid;
 
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String addressNickname;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String receiverName;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String receiverMobileNum;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String zipCode;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String roadAddress;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String jibunAddress;
-    @Column(nullable = false, length = 50)
+    @Column(length = 50)
     private String detailAddress;
-    @Column(nullable = false, length = 5)
+    @Column(length = 50)
     private int defaultAddressCheck;        // 1이 기본 배송지, 0은 기타 배송지
 
 
@@ -42,9 +42,9 @@ public class ShippingAddress extends BaseTimeEntity {
     public ShippingAddress(long shippingAddressSeq, String uuid, String addressNickname, String receiverName, String receiverMobileNum, String zipCode, String roadAddress, String jibunAddress, String detailAddress, int defaultAddressCheck) {
         this.shippingAddressSeq = shippingAddressSeq;
         this.uuid = uuid;
-        this.addressNickname = addressNickname == null ? " " : addressNickname;
-        this.receiverName = receiverName == null ? " " : receiverName;
-        this.receiverMobileNum = receiverMobileNum == null ? " " : receiverMobileNum;
+        this.addressNickname = addressNickname;
+        this.receiverName = receiverName;
+        this.receiverMobileNum = receiverMobileNum;
         this.zipCode = zipCode;
         this.roadAddress = roadAddress;
         this.jibunAddress = jibunAddress;
