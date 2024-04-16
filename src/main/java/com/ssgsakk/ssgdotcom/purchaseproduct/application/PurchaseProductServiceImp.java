9d3549fe5da7827a27269784291c066bfa97c69f
hoontaepark <pthomj13@gmail.com>
2024-04-16 15:02:58 +0900

    refactor:주문시 SoldCount 추가 메소드 수정

--- src/main/java/com/ssgsakk/ssgdotcom/purchaseproduct/application/PurchaseProductServiceImp.java
+++ src/main/java/com/ssgsakk/ssgdotcom/purchaseproduct/application/PurchaseProductServiceImp.java
@@ -82,6 +82,7 @@ public class PurchaseProductServiceImp implements PurchaseProductService {
         }
         purchaseProductRepository.saveAll(purchaseProductList); //저장
         purchaseProductRepositoryImp.decreaseProductStock(productseq, purchaseProductCount);
+        purchaseProductRepositoryImp.increaseProductSoldCount(productseq);
 
     }
 
